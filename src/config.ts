686dcae9095d3e4fd439d9b52c21d278c46da872
f1a3h <ihlwer@outlook.com>
2025-04-16 11:20:29 +0800

    docs: update repo information

--- src/config.ts
+++ src/config.ts
@@ -45,7 +45,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/f1a3h/fuwari", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
@@ -71,7 +71,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/f1a3h/fuwari",
 		},
 	],
 };
